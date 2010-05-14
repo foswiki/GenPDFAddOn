caa9712ac38c85905e36b300e9aa4fc03658f540
GeorgeClark <GeorgeClark@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-05-14 16:31:27 +0000

    Item9008:  Prepare for new release
    git-svn-id: http://svn.foswiki.org/trunk/GenPDFAddOn@7410 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/GenPDFAddOnPlugin.pm
+++ lib/Foswiki/Plugins/GenPDFAddOnPlugin.pm
@@ -65,7 +65,7 @@ our $VERSION = '$Rev: 1340 $';
 # This is a free-form string you can use to "name" your own plugin version.
 # It is *not* used by the build automation tools, but is reported as part
 # of the version number in PLUGINDESCRIPTIONS.
-our $RELEASE = '1.1-rc2';
+our $RELEASE = '1.2';
 
 # Short description of this plugin
 # One line description, is shown in the %SYSTEMWEB%.TextFormattingRules topic:
